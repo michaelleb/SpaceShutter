236c7a0d8bbe6b30272a171ea15049b02e1ee278
markshapiro <markshapiro@10.0.0.2>
2012-11-16 09:05:39 +0200

    ---

---
+++ src/ourproject/messages/StopCutMsg.java
@@ -0,0 +1,19 @@
+package ourproject.messages;
+
+import mark.geometry.*;
+
+public class StopCutMsg implements InterMessage{
+	
+	
+	private Point2D.Short location;
+	
+	public StopCutMsg(Point2D.Short location){
+		this.location=location;
+	}
+	
+	public Point2D.Short getLocation(){return location;}
+	
+	public void getProcessed(MessageProcessing msgprcs){
+		msgprcs.process(this);
+	}
+}
